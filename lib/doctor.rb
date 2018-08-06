3be09b6fdb63b0dd3670e08586244f153550c49a
Tao <werewaswas@live.com>
2018-08-06 19:24:08 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,4 +16,10 @@ class Doctor
   def new_appointment(patient, date)
     Appointment.new(self, patient, date)
   end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
 end
